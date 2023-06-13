baa47527f43df6a34a0da43a5be36b94b7abb0d7
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-06-13 15:07:48 +0200

    first commit

---
+++ js/snack1.js
@@ -0,0 +1,25 @@
+console.log('JS 1 OK');
+
+const vips = [
+  'Brad Pitt',
+  'Johnny Depp',
+  'Lady Gaga',
+  'Cristiano Ronaldo',
+  'Georgina Rodriguez',
+  'Chiara Ferragni',
+  'Fedez',
+  'George Clooney',
+  'Amal Clooney',
+  'Maneskin',
+];
+const placeholder = [];
+
+for (let i = 0; i < vips.length; i++) {
+  placeholder.push({
+    table: 'Tavolo VIP',
+    name: vips[i],
+    place: 'Posto: ' + (i + 1),
+  });
+}
+
+console.table(placeholder);
