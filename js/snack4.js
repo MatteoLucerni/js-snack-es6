41fcc48353350018c82ab9349d45596c52ebfb37
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-06-14 17:01:09 +0200

    added snack 4

--- js/snack4.js
+++ js/snack4.js
@@ -1,2 +1,35 @@
 console.log('---------------------- SNACK 4 -----------------------');
 console.log('JS 4 OK');
+
+const teams = [
+  {
+    nome: 'Inter',
+    'punti fatti': 0,
+    'falli subiti': 0,
+  },
+  {
+    nome: 'Milan',
+    'punti fatti': 0,
+    'falli subiti': 0,
+  },
+  {
+    nome: 'Juve',
+    'punti fatti': 0,
+    'falli subiti': 0,
+  },
+  {
+    nome: 'Torino',
+    'punti fatti': 0,
+    'falli subiti': 0,
+  },
+  {
+    nome: 'Sassuolo',
+    'punti fatti': 0,
+    'falli subiti': 0,
+  },
+  {
+    nome: 'Benevento',
+    'punti fatti': 0,
+    'falli subiti': 0,
+  },
+];
