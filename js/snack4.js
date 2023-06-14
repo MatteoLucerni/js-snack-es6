f3fe4e747480ed551fb51ff648bd4dce546b9ee0
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-06-14 17:01:21 +0200

    added snack  4

--- js/snack4.js
+++ js/snack4.js
@@ -1,6 +1,10 @@
 console.log('---------------------- SNACK 4 -----------------------');
 console.log('JS 4 OK');
 
+// funzione per random numbers inclusi
+const getRandomNumber = (min, max) =>
+  Math.floor(Math.random() * (max + 1 - min)) + min;
+
 const teams = [
   {
     nome: 'Inter',
@@ -33,3 +37,22 @@ const teams = [
     'falli subiti': 0,
   },
 ];
+
+let teamRecivedFouls = [];
+
+teams.forEach(team => {
+  team['punti fatti'] = getRandomNumber(10, 80);
+  team['falli subiti'] = getRandomNumber(1, 20);
+
+  //deconstrutto per stampare il nuovo array
+  let { nome, 'punti fatti': puntiFatti, 'falli subiti': falliSubiti } = team;
+
+  teamRecivedFouls.push({
+    nome,
+    'falli subiti': falliSubiti,
+  });
+});
+
+console.table(teams);
+
+console.table(teamRecivedFouls);
